7e47b33874d83c65ef0d8df7af62b92495a1a4a4
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2024-07-28 21:58:41 +0530

    add

---
+++ Pre-requisites/Getting started with CodeChef Input, output/10.py
@@ -0,0 +1,9 @@
+# Update the '_' in the code below
+
+t = int(input())
+for i in range(t):
+    S = input()
+    #create a variable X which stores the value of string S concatenated with itself
+    X = S + S           
+    #output the variable X
+    print(X)
