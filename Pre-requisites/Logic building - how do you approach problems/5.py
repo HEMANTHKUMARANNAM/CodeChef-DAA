a03333641d10afb5e1cf4c95fd712496383c4abb
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2024-08-02 11:56:48 +0530

    add

---
+++ Pre-requisites/Logic building - how do you approach problems/5.py
@@ -0,0 +1,24 @@
+X = 3
+Y = 4
+Z = 12
+if (Z%X == 0) and (Z%Y == 0):
+    print('Z is divisible by both X and Y')
+elif (Z%X == 0):
+    print('Z is divisible only by X')
+elif (Z%Y == 0):
+    print('Z is divisible only by Y')
+else:
+    print('Z is divisible by neither X nor Y')
+
+X = 3
+Y = 5
+Z = 13
+if (Z%X == 0) and (Z%Y == 0):
+    print('Z is divisible by both X and Y')
+elif (Z%X == 0):
+    print('Z is divisible only by X')
+elif (Z%Y == 0):
+    print('Z is divisible only by Y')
+else:
+    print('Z is divisible by neither X nor Y')
+    
