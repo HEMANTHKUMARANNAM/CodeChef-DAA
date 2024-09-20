7255cff1fdbb2b492dd82ef9b9a0d67c8da95341
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2024-09-20 11:24:07 +0530

    add

--- 7_Backtracking/2_Hamiltonian Circuit Problem/4_Hamiltonian Circuit/Main.java
+++ 7_Backtracking/2_Hamiltonian Circuit Problem/4_Hamiltonian Circuit/Main.java
@@ -2,12 +2,50 @@ import java.util.*;
 
 class main {
 
+    // Check if 'next' vertex can be added after vertex 'v'
+    static boolean check(int v, int next, ArrayList<Integer> circuit, boolean[][] mat) {
+        // Write your code here
+        
+        if( !mat[v][next ] || circuit.indexOf(next)!=-1 )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    static void backtrack(int v, int n, ArrayList<Integer> circuit, ArrayList<ArrayList<Integer>> ans, boolean[][] mat) {
+        if (circuit.size() == n) { // Circuit is completed
+            if (mat[circuit.get(0)][v]) { // Check if cycle is completed, starting should be adjacent to ending
+                circuit.add(circuit.get(0));
+                ans.add(new ArrayList<>(circuit));
+                circuit.remove(circuit.size() - 1);
+            }
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            // Calling the check function
+            if (!check(v, i, circuit, mat))
+                continue;
+
+            // If not visit and is adjacent, add it to our candidate solution
+            circuit.add(i);
+            backtrack(i, n, circuit, ans, mat);
+            circuit.remove(circuit.size() - 1);
+        }
+    }
+
     // Number of vertices and adjacency matrix
     static ArrayList<ArrayList<Integer>> hamiltonianCircuit(int n, boolean[][] mat) {
         ArrayList<Integer> circuit = new ArrayList<>(); // Initially empty circuit
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>(); // To store all the circuits
 
-        // Write your code here
+        for (int i = 1; i <= n; i++) { // Fix the starting vertex
+            circuit.add(i); // Add i to circuit
+            backtrack(i, n, circuit, ans, mat);
+            circuit.remove(circuit.size() - 1); // Remove i from circuit
+        }
 
         return ans;
     }
